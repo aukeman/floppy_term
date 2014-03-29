d9e18c8843083f19be5b837f3cb093a22a4e22be
Jacob Aukeman <jacob.aukeman@gmail.com>
2014-03-29 13:07:58 -0400

    working on drawing pipes

--- flappy_curses.c
+++ flappy_curses.c
@@ -3,6 +3,109 @@
 #include <sys/time.h>
 #include <string.h>
 
+struct pipe_info_t {
+  
+  int opening_top_row_idx;
+  int opening_bottom_row_idx;
+  
+  int left_column_idx;
+  int right_column_idx;
+
+  int upper_pipe_char_offset;
+  int lower_pipe_char_offset;
+
+  int upper_pipe_middle_row_idx;
+  int upper_pipe_end_row_idx;
+  int lower_pipe_end_row_idx;
+  int lower_pipe_middle_row_idx;
+};
+
+const size_t number_of_rows = 30;
+const size_t number_of_columns = 80;
+
+const char* bird = "@@@";
+
+const char* background = 
+  ". . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . \n"
+  " . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .\n"
+  ". . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . \n"
+  " . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .\n"
+  ". . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . \n"
+  " . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .\n"
+  ". . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . \n"
+  " . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .\n"
+  ". . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . \n"
+  " . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .\n"
+  ". . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . \n"
+  " . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .\n"
+  ". . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . \n"
+  " . . . . . . . : : . . . . . . . . . . . . . : : . . . . . . . . . . . . . . . .\n"
+  ". . . : : . : : : : : . . . . . . : : : . : : : : : . . . . . . . : : : . . : : \n"
+  " . : : : : : : : : : : : . . . : : : : : : : : : : : : . . . . : : : : : : : : :\n"
+  ": : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : \n"
+  " : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : :\n"
+  ": : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : \n"
+  " : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : :\n"
+  ": : : : : : : : : : : : : : : : : ,,,,: : : : : : : : : : :,,,,,,,: : : : : : : \n"
+  " : : : : ,,,,,,, : : : : : : :,,,,,,,,,,,: : : : : : : ,,,,,,,,,,,,,,: : : : : :\n"
+  ": : : ,,,,,,,,,,,,, : : : :,,,,,,,,,,,,,,,,,: : : : :,,,,,,,,,,,,,,,,,,,: : : ,,\n"
+  " : ,,,,,,,,,,,,,,,,,,, :,,,,,,,,,,,,,,,,,,,,,,,: ,,,,,,,,,,,,,,,,,,,,,,,,,,: ,,,\n"
+  ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,\n"
+  ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,\n"
+  ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,\n"
+  ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,\n"
+  "################################################################################\n"
+  "################################################################################";
+
+const int floor_height = 2;
+
+const int pipe_width_columns = 8;
+const int pipe_end_height_rows = 2;
+
+const int pipe_gap_height_rows = 7;
+
+const char* upper_pipe_end = "|[____]|\n"
+                             "[[    ]]";
+
+
+const char* lower_pipe_end = "[[____]]\n"
+                             "|[    ]|";
+
+const char* pipe_middle = 
+  "|[    ]|\n"    /* row 0 offset 0 */   
+  "|[    ]|\n"    
+  "|[    ]|\n"
+  "|[    ]|\n"
+  "|[    ]|\n"    
+  "|[    ]|\n"    /* row 5 offset 45 */
+  "|[    ]|\n"
+  "|[    ]|\n"
+  "|[    ]|\n"
+  "|[    ]|\n"    
+  "|[    ]|\n"    /* row 10 offset 90 */
+  "|[    ]|\n"
+  "|[    ]|\n"
+  "|[    ]|\n"
+  "|[    ]|\n"
+  "|[    ]|\n"    /* row 15 offset 135 */
+  "|[    ]|\n"
+  "|[    ]|\n"
+  "|[    ]|\n"
+  "|[    ]|\n"
+  "|[    ]|\n"    /* row 20 offset 180 */
+  "|[    ]|\n"
+  "|[    ]|\n"
+  "|[    ]|\n"
+  "|[    ]|\n"
+  "|[    ]|\n"    /* row 25 offset 225 */
+  "|[    ]|\n"
+  "|[    ]|\n"
+  "|[    ]|\n"
+  "|[    ]|\n"
+  "|[    ]|";     /* row 30 offset 270 */
+
+void initialize_pipe( struct pipe_info_t*, int column_idx );
+
 int main()
 {
   useconds_t render_time = 0;
@@ -10,9 +113,6 @@ int main()
 
   struct timeval now = { 0, 0 }; 
   struct timeval then = { 0, 0 };
-
-  const size_t number_of_rows = 30;
-  const size_t number_of_columns = 80;
 
   float bird_row_fractional_idx = 10.0f;
   const int bird_column_idx = 20;
@@ -26,86 +126,16 @@ int main()
   const float gravity_rows_per_frame_sq = 
     gravity_rows_per_sec_sq * (0.000001f*frame_interval);
 
-  char buffers[number_of_rows][number_of_columns+1];
-
-  const char* bird = "@@@";
-
-  const char* background = 
-    ". . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . \n"
-    " . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .\n"
-    ". . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . \n"
-    " . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .\n"
-    ". . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . \n"
-    " . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .\n"
-    ". . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . \n"
-    " . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .\n"
-    ". . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . \n"
-    " . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .\n"
-    ". . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . \n"
-    " . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . .\n"
-    ". . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . . \n"
-    " . . . . . . . : : . . . . . . . . . . . . . : : . . . . . . . . . . . . . . . .\n"
-    ". . . : : . : : : : : . . . . . . : : : . : : : : : . . . . . . . : : : . . : : \n"
-    " . : : : : : : : : : : : . . . : : : : : : : : : : : : . . . . : : : : : : : : :\n"
-    ": : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : \n"
-    " : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : :\n"
-    ": : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : \n"
-    " : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : : :\n"
-    ": : : : : : : : : : : : : : : : : ,,,,: : : : : : : : : : :,,,,,,,: : : : : : : \n"
-    " : : : : ,,,,,,, : : : : : : :,,,,,,,,,,,: : : : : : : ,,,,,,,,,,,,,,: : : : : :\n"
-    ": : : ,,,,,,,,,,,,, : : : :,,,,,,,,,,,,,,,,,: : : : :,,,,,,,,,,,,,,,,,,,: : : ,,\n"
-    " : ,,,,,,,,,,,,,,,,,,, :,,,,,,,,,,,,,,,,,,,,,,,: ,,,,,,,,,,,,,,,,,,,,,,,,,,: ,,,\n"
-    ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,\n"
-    ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,\n"
-    ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,\n"
-    ",,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,\n"
-    "################################################################################\n"
-    "################################################################################";
-
-  const char* pipe =
-    "[[____]]\n"
-    "|[    ]|\n"
-    "|[    ]|\n"
-    "|[    ]|\n"
-    "|[    ]|\n"
-    "|[    ]|\n"
-    "|[    ]|\n"
-    "|[    ]|\n"
-    "|[    ]|\n"
-    "|[    ]|\n"
-    "|[    ]|\n"
-    "|[    ]|\n"
-    "|[    ]|\n"
-    "|[    ]|\n"
-    "|[    ]|\n"
-    "|[    ]|\n"
-    "|[    ]|\n"
-    "|[    ]|\n"
-    "|[    ]|\n"
-    "|[    ]|\n"
-    "|[    ]|\n"
-    "|[    ]|\n"
-    "|[    ]|\n"
-    "|[    ]|\n"
-    "|[    ]|\n"
-    "|[    ]|\n"
-    "|[    ]|\n"
-    "|[    ]|\n"
-    "|[    ]|\n"
-    "|[____]|\n"
-    "[[    ]]";
-
-
   size_t row_idx = 0;
 
   int keep_looping = 1;
   int flap = 0;
 
-  for ( row_idx = 0;
-	row_idx < number_of_rows;
-	++row_idx ) {
-    buffers[row_idx][number_of_columns] = '\0';
-  }
+  struct pipe_info_t pipes[3];
+
+  initialize_pipe( &pipes[0], 60 );
+  initialize_pipe( &pipes[1], 120 );
+  initialize_pipe( &pipes[2], 160 );
 
   initscr();
   nodelay(stdscr, TRUE);
@@ -131,46 +161,24 @@ int main()
 
     clear();
 
-    /* for ( row_idx = 0; */
-    /* 	  row_idx < number_of_rows; */
-    /* 	  ++row_idx ) { */
-    /*   int pipe_col = 0; */
-
-    /*   memset( buffers[row_idx], bg_char, sizeof(char)*number_of_columns ); */
-
-    /*   /\* for ( pipe_col = col; *\/ */
-    /*   /\* 	    (pipe_col < col+4 && *\/ */
-    /*   /\* 	     pipe_col < number_of_columns); *\/ */
-    /*   /\* 	    ++pipe_col ) { *\/ */
-
-    /*   /\* 	buffers[row_idx][pipe_col] = pipe_char; *\/ */
-    /*   /\* } *\/ */
-	
-
-
-    /*   /\* buffers[row_idx][col] = pipe_char; *\/ */
-    /* } */
-
-    /* for ( row_idx = 0; */
-    /* 	  row_idx < number_of_rows; */
-    /* 	  ++row_idx ) { */
-
-    /*   /\* printf( "%s\n", &buffers[row_idx][0] ); *\/ */
-      
-    /*   mvprintw(row_idx, 0, &buffers[row_idx][0]); */
-    /* } */
-
     mvprintw( 0, 0, background );
 
     mvprintw( (int)(bird_row_fractional_idx + 0.5f), 
 	      bird_column_idx, 
 	      bird );
 
-    mvprintw( 29, 1, "vel: %f  row_idx: %f", 
-	      bird_velocity_rows_per_frame,
-	      bird_row_fractional_idx );
+    if ( 0 < pipes[0].right_column_idx ) {
+      
+      mvprintw( pipes[0].upper_pipe_middle_row_idx,
+		pipes[0].left_column_idx,
+		&pipe_middle[ pipes[0].upper_pipe_char_offset ]);
 
-    /* mvprintw(0, col, "Hello World !!!"); */
+      mvprintw( pipes[0].upper_pipe_end_row_idx,
+		pipes[0].left_column_idx,
+		upper_pipe_end );
+    }
+
+
     refresh();
 
     render_time = 
@@ -181,17 +189,53 @@ int main()
       usleep( frame_interval - render_time );
     }
 
-    /* --col; */
-
     then = now;
 
     int c = getch();
 
     keep_looping = ( 'q' != c );
-    flap =         ( ' ' == c );
+    flap         = ( ' ' == c );
   }
 
   endwin();
 
   return 0;
+}
+
+void initialize_pipe( struct pipe_info_t* pipe_info, int column_idx ) {
+
+  pipe_info->left_column_idx = column_idx;
+  pipe_info->right_column_idx = column_idx + pipe_width_columns;
+
+  pipe_info->opening_top_row_idx = 
+    (rand() % (number_of_rows - 
+	       floor_height - 
+	       pipe_gap_height_rows - 
+	       2*pipe_end_height_rows )) + pipe_end_height_rows;
+
+  pipe_info->opening_bottom_row_idx = (pipe_info->opening_top_row_idx + 
+				       pipe_gap_height_rows);
+
+  pipe_info->upper_pipe_middle_row_idx = 0;
+  pipe_info->upper_pipe_end_row_idx = (pipe_info->opening_top_row_idx -
+				       pipe_end_height_rows);
+
+  pipe_info->lower_pipe_end_row_idx = pipe_info->opening_bottom_row_idx;
+  pipe_info->lower_pipe_middle_row_idx = (pipe_info->opening_bottom_row_idx +
+					  pipe_end_height_rows);
+
+  int number_of_pipe_chars_per_row = (pipe_width_columns+1);
+
+  size_t pipe_middle_str_len = strnlen( pipe_middle, 1024 );
+  size_t number_of_pipe_middle_rows = 
+    (pipe_middle_str_len+1) / number_of_pipe_chars_per_row;
+
+  pipe_info->upper_pipe_char_offset = 
+    ( number_of_pipe_middle_rows - 
+      pipe_info->upper_pipe_end_row_idx ) * number_of_pipe_chars_per_row;
+
+  pipe_info->lower_pipe_char_offset = 
+    ( number_of_rows - 
+      floor_height - 
+      pipe_info->lower_pipe_middle_row_idx ) * number_of_pipe_chars_per_row;
 }
